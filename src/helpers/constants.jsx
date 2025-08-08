21e93490519988ef5365d91e8dedf7887c2c9bee
Vladyslav Popov <vlad_np@ukr.net>
2025-08-08 16:46:08 +0200

    fix url

--- src/helpers/constants.jsx
+++ src/helpers/constants.jsx
@@ -1,8 +1,8 @@
-// export const BASE_URL = 'https://finance-app-novosad.vercel.app/api';
+export const BASE_URL = 'https://finance-app-novosad.vercel.app/api';
 export const BASE_URL_IMG =
   'https://res.cloudinary.com/dfqhj2far/image/upload/v1706095075/bloomskill_event/';
 
-export const BASE_URL = 'http://localhost:3030/api';
+// export const BASE_URL = 'http://localhost:3030/api';
 // export const BASE_URL_IMG = 'https://res.cloudinary.com/dfqhj2far/image/upload/v1706095075/bloomskill_event/';
 export const BASE_URL_AVATAR =
   'https://res.cloudinary.com/dfqhj2far/image/upload/v1706086074/bloomskill_specialist/';
